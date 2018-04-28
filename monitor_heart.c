cd9630ace49542446db9fb427baf8475e36f4fe0
mirandarevans <miranda.r.evans@gmail.com>
2018-04-28 15:15:35 +0000

    reverting to old version

--- monitor_heart.c
+++ monitor_heart.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 
 #define MODIFICATION 3
 
@@ -16,24 +15,12 @@ int main(int ac, char **av)
 	char **script;
 	extern char **environ;
 	int status;
-	int bytes_read;
-	int fd;
-	char buffer1[10];
-	char buffer2[10];
 
 	if (ac != 3)
 		exit(EXIT_FAILURE);
 
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
+	if (stat(av[1], &before) < 0)
 		exit(EXIT_FAILURE);
-
-	bytes_read = read(fd, buffer1, 9);
-	if (bytes_read < 0)
-		exit(EXIT_FAILURE);
-	buffer1[0] = '\0';
-
-	close(fd);
 
 	script = malloc(sizeof(char *) * 2);
 	script[0] = av[2];
@@ -51,18 +38,12 @@ int main(int ac, char **av)
 	wait(&status);
 	free(script);
 
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
+	if (stat(av[1], &after) != 0)
 		return (MODIFICATION);
 
-	bytes_read = read(fd, buffer2, 9);
-	if (bytes_read < 0)
-		exit(EXIT_FAILURE);
-	buffer2[9] = '\0';
-
-	close(fd);
-
-	if (strcmp(buffer1, buffer2) != 0)
+	if (before.st_atime != after.st_atime ||
+	    before.st_mtime != after.st_mtime ||
+	    before.st_ctime != after.st_ctime)
 		return (MODIFICATION);
 
 	return (0);
