1df0a008d8c80b4e8f6eaf10f2c4d38aae7b2fa7
root <root@ip-172-31-40-68.us-east-2.compute.internal>
2018-06-16 20:35:01 +0000

    backed up version one, initializing version two

--- formula/lib/python3.5/site-packages/swagger_spec_validator-2.1.0-py3.5.egg/swagger_spec_validator/__init__.py
+++
@@ -1,7 +0,0 @@
-from swagger_spec_validator.util import validate_spec_url
-from swagger_spec_validator.common import SwaggerValidationError
-
-__all__ = [
-    'SwaggerValidationError',
-    'validate_spec_url',
-]
--- samples/benevolent_1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("You look nice today!\n");
-
-	return (0);
-}
